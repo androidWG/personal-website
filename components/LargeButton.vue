b773f27d74265fe3ffb22b248ef24e6ecf9e2166
Sam Rodrigues <sam.roliveira@proton.me>
2022-11-14 19:30:06 -0500

    Added base CSS and buttons

---
+++ components/LargeButton.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props: {
+        link: String,
+        text: String,
+        icon: String
+    }
+})
+</script>
+
+<template>
+<a class="large button" :href="link">
+    <img v-if="icon !== undefined" class="icon" :src="require(`@/assets/icons/${icon}.svg`)" alt="">
+    <p v-if="text !== undefined" class="text">{{text}}</p>
+</a>
+</template>
+
+<style lang="scss" src="./button.scss"></style>
---
+++ components/SmallButton.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props: {
+        link: String,
+        text: String,
+        icon: String,
+        rounded: Boolean
+    }
+})
+</script>
+
+<template>
+<a :class="(rounded)?'button small rounded':'button small'" :href="link">
+    <img v-if="icon !== undefined" class="icon" :src="require(`@/assets/icons/${icon}.svg`)" alt="">
+    <p v-if="text !== undefined" class="text">{{text}}</p>
+</a>
+</template>
+
+<style lang="scss" src="./button.scss"></style>
