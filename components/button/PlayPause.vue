c8e13cbf3eacb360a7a026a6a2f4c1eb7c8e2916
Sam Rodrigues <sam.roliveira@proton.me>
2022-11-15 18:18:11 -0500

    Updated buttons

---
+++ components/button/PlayPause.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="play-icon">
+    <img v-if="!isPlaying" src="@/assets/icons/play.svg" alt="Play sample" />
+    <img v-if="isPlaying" src="@/assets/icons/stop.svg" alt="Stop playing sample" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data() {
+        return {
+            isPlaying: false
+        }
+    }
+})
+</script>
+
+<style>
+.play-icon {
+    width: 64px;
+    height: 64px;
+    position: relative;
+}
+</style>
