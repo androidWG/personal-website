1857aa5e1fd2f664129fe6d00706a67809b3598a
Sam Rodrigues <sam.roliveira@proton.me>
2022-11-14 19:31:33 -0500

    Added separator and base sections

---
+++ components/MusicSection.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="music-group">
+            <h2>Music</h2>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>
+
+<style lang="scss">
+    .music-group {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 0px;
+        gap: 16px;
+
+        align-self: stretch;
+    }
+</style>
---
+++ components/Separator.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="vl"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+    @use '@/assets/css/main.scss';
+
+    .vl {
+        border: 2px solid main.$gray-medium;
+        height: 52px;
+    }
+</style>
