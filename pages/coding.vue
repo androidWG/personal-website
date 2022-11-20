271774de10c366f32b44d11eccd4f6a6012b6363
Sam Rodrigues <sam.roliveira@proton.me>
2022-11-20 17:47:13 -0500

    Added pages

---
+++ pages/coding.vue
@@ -0,0 +1,19 @@
+<template>
+  <Page>
+    <HomeSection/>
+  </Page>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import HomeSection from '~/components/sections/HomeSection.vue';
+import Page from '~/components/page/Page.vue';
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: {
+    HomeSection,
+    Page
+  }
+})
+</script>
---
+++ pages/music.vue
@@ -0,0 +1,19 @@
+index.vue<template>
+  <Page selected="music">
+    <MusicSection/>
+  </Page>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import MusicSection from '~/components/sections/MusicSection.vue';
+import Page from '~/components/page/Page.vue';
+
+export default Vue.extend({
+  name: 'MusicPage',
+  components: {
+    MusicSection,
+    Page
+  }
+})
+</script>
