9b3070c08a1b15f9eb051344d9c2ffde68ce85c8
Sam Rodrigues <sam.roliveira@proton.me>
2022-11-27 13:09:14 -0500

    Improved button test page

--- pages/btn-test.vue
+++ pages/btn-test.vue
@@ -1,21 +1,22 @@
 <template>
   <div>
     <h2>Large Buttons</h2>
-    <div style="display: flex">
-      <LargeButton :compact="false" text="Test" icon="github"></LargeButton>
-      <LargeButton :compact="true" text="Test" icon="github"></LargeButton>
-      <LargeButton :compact="false" icon="github"></LargeButton>
-      <LargeButton :compact="true" icon="github"></LargeButton>
-      <LargeButton :compact="false" text="Test"></LargeButton>
-      <LargeButton :compact="true" text="Test"></LargeButton>
+    <div class="container">
+      <LargeButton text="Test" icon="github"></LargeButton>
+      <LargeButton icon="github"></LargeButton>
+      <LargeButton text="Test"></LargeButton>
     </div>
     <h2>Small Buttons</h2>
-    <div style="display: flex; gap: 8px;">
-      <SmallButton :rounded="false" text="Test" icon="github"></SmallButton>
-      <SmallButton :rounded="true" text="Test" icon="github"></SmallButton>
+    <h3>not rounded</h3>
+    <div class="container">
       <SmallButton :rounded="false" icon="github"></SmallButton>
-      <SmallButton :rounded="true" icon="github"></SmallButton>
+      <SmallButton :rounded="false" text="Test" icon="github"></SmallButton>
       <SmallButton :rounded="false" text="Test"></SmallButton>
+    </div>
+    <h3>rounded</h3>
+    <div class="container">
+      <SmallButton :rounded="true" icon="github"></SmallButton>
+      <SmallButton :rounded="true" text="Test" icon="github"></SmallButton>
       <SmallButton :rounded="true" text="Test"></SmallButton>
     </div>
   </div>
@@ -34,3 +35,13 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style scoped lang="scss">
+.container {
+  background-color: #2E282A;
+
+  margin: 16px 0;
+  display: flex;
+  gap: 16px;
+}
+</style>
