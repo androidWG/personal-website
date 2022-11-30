733416075323126a55d0f44bc3bd31d183f98887
Sam Rodrigues <sam.roliveira@proton.me>
2022-11-29 22:20:06 -0500

    Added coding page and data

--- pages/coding.vue
+++ pages/coding.vue
@@ -1,18 +1,43 @@
 <template>
   <div>
-    <h2>Coding</h2>
+    <h2>Coding Projects</h2>
+    <div class="group">
+      <CodingItem
+        v-for="c in data.projects"
+        :title="c.name"
+        :status="c.status"
+        :image="c.image"
+        :date-start="c.dates.start"
+        :date-end="c.dates.finish"
+        :using="c.using"
+        :for="c.for"
+        :key="c.name"
+      >
+        {{ c.description }}
+      </CodingItem>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
+import CodingItem from "~/components/items/CodingItem.vue";
 
 export default Vue.extend({
-  name: 'IndexPage',
+  components: {CodingItem},
   head() {
     return {
-      title: "Sam Rodrigues | Coding"
-    }
+      title: "Sam Rodrigues | Coding",
+    };
   },
-})
+  async asyncData({$content, params}) {
+    const data = await $content("coding", params.slug).fetch();
+    return {
+      data,
+    };
+  },
+  created() {
+    this.$emit('setSkinny', true);
+  }
+});
 </script>
