659cb425a4737525101dd11f474bb97e0ef72d0c
Anna Rodrigues <anna.roliveira@proton.me>
2023-03-28 15:37:47 +0000

    Updated email and added previous name

--- components/Links.vue
+++ components/Links.vue
@@ -1,35 +1,39 @@
 <template>
-  <div class="links h-list">
-    <Button
-      :small="true"
-      text="me@samrodrigues.net"
-      icon="mail"
-      link="mailto:me@samrodrigues.net"
-    />
-    <div class="sub">
-      <Button :small="true" icon="github" link="https://github.com/androidWG" />
-      <Button
-        :small="true"
-        icon="dribbble"
-        link="https://dribbble.com/androidWG"
-      />
-      <Button
-        :small="true"
-        icon="soundcloud"
-        link="https://soundcloud.com/androidWG"
-      />
-      <Button
-        :small="true"
-        icon="bandcamp"
-        link="https://androidwg.bandcamp.com"
-      />
-      <Button
-        :small="true"
-        icon="linkedin"
-        link="https://www.linkedin.com/in/anna-roliveira/"
-      />
+    <div class="links h-list">
+        <Button
+            :small="true"
+            text="me@annarodrigues.net"
+            icon="mail"
+            link="mailto:me@annarodrigues.net"
+        />
+        <div class="sub">
+            <Button
+                :small="true"
+                icon="github"
+                link="https://github.com/androidWG"
+            />
+            <Button
+                :small="true"
+                icon="dribbble"
+                link="https://dribbble.com/androidWG"
+            />
+            <Button
+                :small="true"
+                icon="soundcloud"
+                link="https://soundcloud.com/androidWG"
+            />
+            <Button
+                :small="true"
+                icon="bandcamp"
+                link="https://androidwg.bandcamp.com"
+            />
+            <Button
+                :small="true"
+                icon="linkedin"
+                link="https://www.linkedin.com/in/anna-roliveira/"
+            />
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -37,9 +41,9 @@ import Vue from "vue";
 import Button from "./button/Button.vue";
 
 export default Vue.extend({
-  components: {
-    Button,
-  },
+    components: {
+        Button,
+    },
 });
 </script>
 
@@ -48,22 +52,22 @@ export default Vue.extend({
 @use "@/assets/css/breakpoints.scss";
 
 .links {
-  flex-wrap: wrap;
-  width: fit-content;
-  padding: 0 32px;
+    flex-wrap: wrap;
+    width: fit-content;
+    padding: 0 32px;
 
-  @include breakpoints.q-small {
-    gap: 8px;
-  }
+    @include breakpoints.q-small {
+        gap: 8px;
+    }
 }
 
 .sub {
-  display: flex;
-  width: fit-content;
-  gap: 32px;
+    display: flex;
+    width: fit-content;
+    gap: 32px;
 
-  @include breakpoints.q-small {
-    gap: 8px;
-  }
+    @include breakpoints.q-small {
+        gap: 8px;
+    }
 }
 </style>
